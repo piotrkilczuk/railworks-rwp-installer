a8f8c841bcab7fd3e949e1a6638ac6bd45c3d326
Piotr Kilczuk <piotr@tymaszweb.pl>
2016-10-15 19:31:19 +0100

    Hallo world EXE app

---
+++ make.bat
@@ -0,0 +1,1 @@
+.\venv\Scripts\pyinstaller.exe rwp.py --onefile --console --name rwpinstaller
---
+++ rwp.py
@@ -0,0 +1,6 @@
+import time
+
+
+if __name__ == '__main__':
+    print('Hello world!')
+    time.sleep(5)
