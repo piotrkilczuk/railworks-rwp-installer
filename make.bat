907a9514bbc764b74dbd04d6f8773714f2a086d0
Piotr Kilczuk <piotr@tymaszweb.pl>
2016-10-17 21:43:35 +0100

    Add an icon

--- make.bat
+++ make.bat
@@ -1,1 +1,9 @@
-.\venv\Scripts\pyinstaller.exe rwp.py --onefile --console --name rwpinstaller
+mkdir build
+
+REM Make the icon
+convert media\icon256.png media\icon128.png media\icon64.png media\icon32.png build\icon.ico
+
+REM Build EXE
+.\venv\Scripts\pyinstaller.exe rwp.py --onefile --console --name rwpinstaller --icon build\icon.ico
+
+REM @TODO: sign digitally
